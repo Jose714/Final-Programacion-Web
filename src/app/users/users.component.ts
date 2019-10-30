f24f260368a166d67991fbbc8cc0bd73b43d5217
Nixon Javier Ochoa Perdomo <32244010+NixonOchoa@users.noreply.github.com>
2019-10-30 11:42:47 -0500

    Mejora en el user

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -6,8 +6,10 @@ import {LoginComponent} from '../login/login.component'
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
+
 export class UsersComponent implements OnInit {
  user = JSON.parse(localStorage.getItem("email"));
+ 
   constructor() { }
 
   ngOnInit() {
